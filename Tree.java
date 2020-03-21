ac2cb836abf4614128b90748ae152b86d3103160
Kyle Byse <kbyse231@gmail.com>
2020-03-21 19:59:15 -0400

    Add files via upload
    This is a small simple Java project, as a beginner.

---
+++ Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    //I am going to introduce myseslf and plant a tree
+    System.out.println("I'm Kyle. I'm learning Java.");
+    System.out.println("I am planting a tree.");
+    System.out.println("       ");
+    System.out.println("   *   ");
+    System.out.println("  ***  ");
+    System.out.println(" ***** ");
+    System.out.println("*******");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+    System.out.println("   *   ");
+  }  
+}
